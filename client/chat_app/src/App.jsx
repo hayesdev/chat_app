6bac71b29c29a61aad82436e2bcac4eea9fe2dc8
Greg Hayes <gthelight@gmail.com>
2022-11-10 16:42:37 -0500

    set up imports from index.js

--- client/chat_app/src/App.jsx
+++ client/chat_app/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const apiKey = "hpcah44z5959";
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>chat app</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
