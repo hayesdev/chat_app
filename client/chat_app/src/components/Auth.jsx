55e59c9bf1c92b07ee730aef41a7278a9053a50d
Greg Hayes <gthelight@gmail.com>
2022-11-10 20:00:56 -0500

    auth page setup

---
+++ client/chat_app/src/components/Auth.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
+
+import signinImage from "../assets/signup.jpg";
+
+const Auth = () => {
+  const [isSignup, setIsSignup] = useState(true);
+
+  const handleChange = () => {};
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+  };
+
+  return (
+    <div className="auth__form-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+          <form action="" onSubmit={() => {}}>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  type="text"
+                  name="fullName"
+                  placeholder="Full Name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="avatarURL">Avatar URL</label>
+                <input
+                  type="text"
+                  name="avatarURL"
+                  placeholder="Avatar URL"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+          </form>
+          <div className="aut_form-container_fields-account">
+            <p>
+              {isSignup
+                ? "Already have an account? "
+                : "Don't have an account? "}
+              <span
+                onClick={switchMode}
+                style={{ cursor: "pointer", textDecoration: "underline" }}
+              >
+                {isSignup ? "Sign In" : "Sign Up"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
